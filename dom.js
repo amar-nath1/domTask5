fdfe09230a96bd5720f371ae70de6b9eb9d1e8a6
amar-nath1 <amarat279955>
2022-10-01 17:41:09 +0530

    adding dom js

---
+++ dom.js
@@ -0,0 +1,6 @@
+// Here the background-color of list items will be yellow (By class name)
+let listitems=document.getElementsByClassName('list-group-item')
+
+for (let li of listitems){
+li.style.backgroundColor='palegoldenrod'
+}
