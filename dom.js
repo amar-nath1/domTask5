d0fc849473a26e31aa329c1826e58ef3a1e44e88
amar-nath1 <amarat279955>
2022-10-01 17:51:04 +0530

    dom changes via tag name

--- dom.js
+++ dom.js
@@ -1,6 +1,15 @@
 // Here the background-color of list items will be yellow (By class name)
-let listitems=document.getElementsByClassName('list-group-item')
+// here bg color of new last item will not be updated as it has diff class name
+// let listitems=document.getElementsByClassName('list-group-item')
 
-for (let li of listitems){
-li.style.backgroundColor='palegoldenrod'
+// for (let li of listitems){
+// li.style.backgroundColor='palegoldenrod'
+// }
+
+// changing bg color of list item via tag name
+
+let li=document.getElementsByTagName('li')
+
+for (let i of li){
+i.style.backgroundColor='palegoldenrod'
 }
